2d52e09f9c6246532fd48453dd2368d96b8af08f
Marc <marc.gluyas@gmail.com>
2018-02-20 17:41:42 +1300

    Added base puzzle impl. Util additions, refactorings

--- src/main.rs
+++ src/main.rs
@@ -4,78 +4,50 @@ use util::*;
 mod world;
 use world::*;
 
+mod puzzle;
+use puzzle::*;
+
 use std::io;
 use std::error::Error;
 
 fn main() {
-    let room = {
-        let mut room = Room {
-            tiles: Grid::default((4, 4)),
-        };
-
-        for x in 0..room.size().x  {
-            for y in 0..room.size().y {
-                room.tiles[(x, y)] = Option::Some(
-                    Tile {
-                        material: Material::Grass,
-                        prop: None,
-                        elevation: 0,
-                    }
-                );
-            }
-        }
-
-        room.tiles[(1, 0)].as_mut().unwrap().prop = Some(Prop::Rock);
-        room.tiles[(2, 2)].as_mut().unwrap().prop = Some(Prop::Rock);
-        room
-    };
+    let room = make_room();
+    let mut puzzle = make_puzzle();
 
     let mut player = Pos::default();
 
-    let mut move_player = |dir| {
-        {
-            let new_pos = player.nudge(dir, room.size());
-
-            if room.tiles[new_pos].is_some()
-            && room.tiles[new_pos].as_ref().unwrap().prop.is_none()
-            {
-                player = new_pos;
-            }
-        }
-
-        for y in 0..room.size().x {
-            for x in 0..room.size().y {
-                if player == (x, y) {
-                    print!("!");
-                } else if room.tiles[(x, y)].is_some() {
-                    if room.tiles[(x, y)].as_ref().unwrap().prop.is_some() {
-                        print!("o");
-                    } else {
-                        print!("_");
-                    }
-                } else {
-                    print!(" ");
-                }
-            }
-            println!();
-        }
-    };
-
     let mut input = String::new();
     let stdin = io::stdin();
+
+    let mut next_input_spell_dir = false;
+
+    render(player, &room, &puzzle);
 
     'main: loop {
         match stdin.read_line(&mut input) {
             Ok(_) => {
                 for c in input.chars() {
-                    match c {
-                        'w' => move_player(Direction::North),
-                        'd' => move_player(Direction::East),
-                        's' => move_player(Direction::South),
-                        'a' => move_player(Direction::West),
-                        'q' => break 'main,
-                        _   => {},
-                    };
+                    if !next_input_spell_dir {
+                        match c {
+                            'w' => move_player(Direction::North, &mut player, &room, &puzzle),
+                            'd' => move_player(Direction::East, &mut player, &room, &puzzle),
+                            's' => move_player(Direction::South, &mut player, &room, &puzzle),
+                            'a' => move_player(Direction::West, &mut player, &room, &puzzle),
+                            'c' => next_input_spell_dir = true,
+                            'q' => break 'main,
+                            _   => {},
+                        };
+                    } else {
+                        next_input_spell_dir = false;
+                        match c {
+                            'w' => cast_spell(Direction::North, &player, &room, &mut puzzle),
+                            'd' => cast_spell(Direction::East, &player, &room, &mut puzzle),
+                            's' => cast_spell(Direction::South, &player, &room, &mut puzzle),
+                            'a' => cast_spell(Direction::West, &player, &room, &mut puzzle),
+                            'q' => break 'main,
+                            _   => next_input_spell_dir = true,
+                        };
+                    }
                 }
                 input.clear();
             },
@@ -85,4 +57,132 @@ fn main() {
             },
         }
     }
+}
+
+fn move_player(dir: Direction, player: &mut Pos, room: &Room, puzzle: &PuzzleGrid) {
+    if let Some(new_pos) = player.step(dir, room.size()) {
+        if room.tiles[new_pos].is_some()
+        && room.tiles[new_pos].as_ref().unwrap().prop.is_none()
+        && puzzle.cells[new_pos].as_ref()
+            .map_or(true, |cell| { cell.plant.is_none() })
+        {
+            *player = new_pos;
+        }
+    }
+
+    render(*player, room, puzzle);
+}
+
+fn cast_spell(dir: Direction, player: &Pos, room: &Room, puzzle: &mut PuzzleGrid) {
+    let mut puzzle_temp = puzzle.clone();
+
+    for pos in player.iter_line(dir, room.size()) {
+        // check that spell can pass through terrain
+        if !room.tiles[pos].as_ref().map(|tile| {
+            match tile.material {
+                Material::Grass | Material::Dirt => true,
+                _ => false
+            }
+        }).unwrap_or(false) {
+            break;
+        }
+
+        let mut absorbed = false;
+
+        if puzzle_temp.cells[pos].is_some() {
+            let cell = puzzle_temp.cells[pos].as_mut().unwrap();
+
+            if let Some(_plant) = cell.plant.as_ref() {
+                // TODO: implement plant specific behaviour here
+                cell.hits += 1;
+            };
+
+            let virgin = match puzzle_temp.regions[cell.region] {
+                PuzzleStatus::Virgin => true,
+                _ => false,
+            };
+
+            if cell.has_wall(dir) && virgin {
+                puzzle_temp.regions[cell.region] = PuzzleStatus::Exhausted;
+                absorbed = true;
+            }
+        }
+        if absorbed {
+            *puzzle = puzzle_temp;
+            break;
+        }
+    }
+
+    render(*player, room, puzzle);
+}
+
+fn render(player: Pos, room: &Room, puzzle: &PuzzleGrid) {
+    for y in 0..room.size().x {
+        for x in 0..room.size().y {
+            if player == (x, y) {
+                print!("!");
+            } else if let Some(cell) = puzzle.cells[(x, y)].as_ref() {
+                if let Some(_plant) = cell.plant.as_ref() {
+                    if cell.is_sprouted() {
+                        print!("{}", cell.hits);
+                    } else {
+                        print!("*");
+                    }
+                } else {
+                    if let PuzzleStatus::Virgin = puzzle.regions[cell.region] {
+                        print!("{}", cell.get_symbol_virgin());
+                    } else {
+                        print!("{}", cell.get_symbol_exhausted());
+                    }
+                }
+            } else if let Some(tile) = room.tiles[(x, y)].as_ref() {
+                if tile.prop.is_some() {
+                    print!("o");
+                } else {
+                    print!(".");
+                }
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
+}
+
+fn make_puzzle() -> PuzzleGrid {
+    let mut puzzle = PuzzleGrid {
+        cells: Grid::of_default((4, 4)),
+        regions: vec![PuzzleStatus::default()],
+    };
+
+    puzzle.set_cell((3,0), Some(PuzzleCell::new(0, None)));
+    puzzle.set_cell((2,0), Some(PuzzleCell::new(0, None)));
+    puzzle.set_cell((3,1), Some(PuzzleCell::new(0, Some(Plant::Default))));
+    puzzle.set_cell((2,1), Some(PuzzleCell::new(0, None)));
+    puzzle.set_cell((1,1), Some(PuzzleCell::new(0, Some(Plant::Default))));
+
+    puzzle
+}
+
+fn make_room() -> Room {
+    let mut room = Room {
+        tiles: Grid::of_default((4, 4)),
+    };
+
+    for x in 0..room.size().x  {
+        for y in 0..room.size().y {
+            room.tiles[(x, y)] = Option::Some(
+                Tile {
+                    material: Material::Grass,
+                    prop: None,
+                    elevation: 0,
+                }
+            );
+        }
+    }
+
+    room.tiles[(1, 0)].as_mut().unwrap().prop = Some(Prop::Rock);
+    room.tiles[(3, 3)].as_mut().unwrap().prop = Some(Prop::Rock);
+    room.tiles[(1, 3)] = None;
+    room
 }
--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,29 @@
 use std::ops::{Index, IndexMut};
 
+#[derive(Copy, Clone)]
 pub enum Direction {
     North = 0,
     East  = 1,
     South = 2,
     West  = 3,
+}
+
+impl Direction {
+    pub fn all() -> &'static [Direction] {
+        &[Direction::North, Direction::East, Direction::South, Direction::West]
+    }
+    pub fn opposite(self) -> Direction {
+        match self {
+            Direction::North => Direction::South,
+            Direction::East  => Direction::West,
+            Direction::South => Direction::North,
+            Direction::West  => Direction::East,
+        }
+    }
+
+    pub fn as_flag(self) -> u8 {
+        1 << (self as u8)
+    }
 }
 
 #[derive(Copy, Clone, Debug, Default, Eq)]
@@ -14,27 +33,72 @@ pub struct Pos {
 }
 
 impl Pos {
-    pub fn nudge <P: Into<Pos>> (mut self, dir: Direction, bound: P) -> Pos {
-        fn sub_bounded(val: &mut u8) {
-            if *val > 0 { *val -= 1 };
-        }
+    pub fn step <P: Into<Pos>> (mut self, dir: Direction, bound: P) -> Option<Pos> {
+        let sub_bounded = |val: &mut u8| -> bool {
+            if *val > 0 { *val -= 1; true } else { false }
+        };
 
-        fn add_bounded(val: &mut u8, bound: &u8) {
-            if *val < bound - 1 { *val += 1 };
-        }
+        let add_bounded = |val: &mut u8, bound: &u8| -> bool {
+            if *val < bound - 1 { *val += 1; true } else { false }
+        };
 
         let bound = bound.into();
-        match dir {
+        if match dir {
             Direction::North => sub_bounded(&mut self.y),
             Direction::East  => add_bounded(&mut self.x, &bound.x),
             Direction::South => add_bounded(&mut self.y, &bound.y),
             Direction::West  => sub_bounded(&mut self.x),
+        } {
+            Some(self)
+        } else {
+            None
+        }
+    }
+
+    pub fn snap_to_edge <P: Into<Pos>> (mut self, dir: Direction, bounds: P) -> Pos {
+        let bounds = bounds.into();
+        match dir {
+            Direction::North => self.y = 0,
+            Direction::East  => self.x = bounds.x - 1,
+            Direction::South => self.y = bounds.y - 1,
+            Direction::West  => self.x = 0,
         }
         self
+    }
+
+    pub fn iter_line <P: Into<Pos>> (self, dir: Direction, bounds: P) -> LineIterator {
+        LineIterator::new(self, dir, bounds)
+    }
+
+    pub fn iter_rect(self, dir_primary: Direction, dir_secondary: Direction) -> RectIterator {
+        RectIterator::new(self, dir_primary, dir_secondary)
+    }
+
+    pub fn contains <P: Into<Pos>> (self, other: P) -> bool {
+        let other = other.into();
+        self.x > other.x && self.y > other.y
+    }
+
+    #[inline]
+    pub fn is_within <P: Into<Pos>> (self, bound: P) -> bool {
+        bound.into().contains(self)
+    }
+
+    pub fn component_add <P: Into<Pos>> (self, other: P) -> Pos {
+        let other = other.into();
+        (self.x + other.x, self.y + other.y).into()
+    }
+
+    pub fn component_diff <P: Into<Pos>> (self, other: P) -> Pos {
+        let diff = |a, b| if a >= b { a - b } else { b - a };
+
+        let other = other.into();
+        (diff(self.x, other.x), diff(self.y, other.y)).into()
     }
 }
 
 impl From<(u8, u8)> for Pos {
+    #[inline]
     fn from(pos: (u8, u8)) -> Self {
         Pos { x: pos.0, y: pos.1 }
     }
@@ -47,6 +111,99 @@ impl <P> PartialEq<P> for Pos where P: Into<Pos> + Copy {
     }
 }
 
+pub struct LineIterator {
+    next: Pos,
+    dir: Direction,
+    bound: Pos,
+}
+
+impl Iterator for LineIterator {
+    type Item = Pos;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.bound == (0, 0) {
+            None
+        } else {
+            let val = self.next;
+
+            if let Some(step) = self.next.step(self.dir, self.bound) {
+                self.next = step;
+            } else {
+                self.bound = (0, 0).into();
+            }
+            Some(val)
+        }
+    }
+}
+
+impl LineIterator {
+    pub fn new <P0: Into<Pos>, P1: Into<Pos>> (origin: P0, dir: Direction, bound: P1) -> LineIterator {
+        let origin = origin.into();
+
+        let bound = {
+            let bound = bound.into();
+            if bound.contains(origin) {
+                bound
+            } else {
+                (0, 0).into()
+            }
+        };
+
+        LineIterator { next: origin, dir, bound }
+    }
+}
+
+pub struct RectIterator {
+    primary: LineIterator,
+    secondary: LineIterator,
+}
+
+impl Iterator for RectIterator {
+    type Item = Pos;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.primary.next();
+
+        if next.is_none() {
+            self.secondary.next().map(|next| {
+                self.primary = LineIterator {
+                    next,
+                    dir: self.primary.dir,
+                    bound: self.secondary.bound
+                };
+                self.primary.next()
+            }).unwrap_or_default()
+        } else {
+            next
+        }
+    }
+}
+
+impl RectIterator {
+    pub fn new <P: Into<Pos>> (range: P, dir_primary: Direction, dir_secondary: Direction) -> RectIterator {
+        if (dir_primary as u8 + dir_secondary as u8) % 2 == 0 {
+            panic!("Attempted to make RectIterator with non-orthogonal directions")
+        }
+        let range = range.into();
+
+        let origin = Pos::default()
+            .snap_to_edge(dir_primary.opposite(), range)
+            .snap_to_edge(dir_secondary.opposite(), range);
+
+        let mut secondary = LineIterator::new(origin, dir_secondary, range);
+
+        // pull the first value from secondary so the next time it is called it yields origin + dir
+        let primary = if let Some(origin) = secondary.next() {
+            LineIterator::new(origin, dir_primary, range)
+        } else {
+            LineIterator::new(origin, dir_secondary, (0, 0))
+        };
+
+        RectIterator { primary, secondary }
+    }
+}
+
+#[derive(Clone, Default)]
 pub struct Grid<T> {
     data: Vec<T>,
     size: Pos,
@@ -68,7 +225,7 @@ impl <T> Grid<T> {
 }
 
 impl <T: Default> Grid<T> {
-    pub fn default <P: Into<Pos>> (size: P) -> Grid<T> {
+    pub fn of_default <P: Into<Pos>> (size: P) -> Grid<T> {
         let size = size.into();
         let capacity = (size.x * size.y) as usize;
 
